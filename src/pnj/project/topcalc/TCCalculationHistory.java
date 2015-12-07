4ce3b904dcdb61e352531e15d7b4db8e84fde8f0
Jason Paolasini <Jason.Paolasini@mcmaster.ca>
2015-12-07 12:29:53 -0500

    Cleanup Calculation History

--- src/pnj/project/topcalc/TCCalculationHistory.java
+++ src/pnj/project/topcalc/TCCalculationHistory.java
@@ -2,12 +2,8 @@ package pnj.project.topcalc;
 
 import java.util.ArrayList;
 
-public class TCCalculationHistory {
-	static ArrayList<TCCurrentCalculation> history;
-	int indexToDisplay;
-	
 	public TCCalculationHistory(){
 		history = new ArrayList<TCCurrentCalculation>();
 	}
 
-}
+
