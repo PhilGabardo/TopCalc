b3222f855ff3cef885d75387063f9310bc2299fa
PhilGabardo <gabardpj@mcmaster.ca>
2015-12-07 13:47:02 -0500

    button constructor test
    see title

--- src/pnj/project/topcalc/DeleteButton.java
+++ src/pnj/project/topcalc/DeleteButton.java
@@ -1,22 +1,37 @@
 package pnj.project.topcalc;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.widget.Button;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.View.OnClickListener;
 
-public class DeleteButton extends TCButton implements OnClickListener{
+public class DeleteButton extends Button implements OnClickListener{
 
 
-	public DeleteButton(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		// TODO Auto-generated constructor stub
+	public DeleteButton(Context context) {
+	    super(context);
+	}
+
+	public DeleteButton(Context context, AttributeSet attrs) {
+	    super(context, attrs);
+	}
+
+	public DeleteButton(Context context, AttributeSet attrs, int defStyleAttr) {
+	    super(context, attrs, defStyleAttr);
+	}
+
+	@TargetApi(21)
+	public DeleteButton(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+	    super(context, attrs, defStyleAttr);
 	}
 
 	@Override
-	public void action() {
-		Calculator.currentCalculation.deleteLast();
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
 	}
+
 
 }
