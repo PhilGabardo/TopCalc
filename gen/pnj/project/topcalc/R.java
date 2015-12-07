a2f2c5dccad109792b32e4b6a4c150dd6e401a92
Jason Paolasini <Jason.Paolasini@mcmaster.ca>
2015-12-07 13:58:09 -0500

    move to linear layout

--- gen/pnj/project/topcalc/R.java
+++ gen/pnj/project/topcalc/R.java
@@ -24,10 +24,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int DeleteButton=0x7f08000b;
+        public static final int DisplayBox=0x7f080007;
+        public static final int MainCalc=0x7f08000a;
+        public static final int OuterCalc=0x7f080006;
+        public static final int PrevCalc=0x7f080009;
+        public static final int PrevnCurrentBox=0x7f080008;
+        public static final int action_settings=0x7f08000c;
+        public static final int linearLayout1=0x7f080000;
+        public static final int tableRow1=0x7f080002;
+        public static final int tableRow2=0x7f080003;
+        public static final int tableRow3=0x7f080004;
+        public static final int tableRow4=0x7f080005;
+        public static final int tableRow5=0x7f080001;
     }
     public static final class layout {
         public static final int activity_calculator=0x7f030000;
+        public static final int calculator_layout=0x7f030001;
     }
     public static final class menu {
         public static final int calculator=0x7f070000;
--- src/pnj/project/topcalc/Calculator.java
+++ src/pnj/project/topcalc/Calculator.java
@@ -17,8 +17,7 @@ public class Calculator extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_calculator);
-		currentCalculation = new TCCurrentCalculation(this);
+		setContentView(R.layout.calculator_layout);
 	}
 
 	@Override
