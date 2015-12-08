7d41af759b6da454452eaf14639bf3d1d2c4134c
PhilGabardo <gabardpj@mcmaster.ca>
2015-12-08 13:01:51 -0500

    ACTUALLY added behaviour for onclick for TCTermBut
    see title.

--- src/pnj/project/topcalc/TCTermButton.java
+++ src/pnj/project/topcalc/TCTermButton.java
@@ -2,9 +2,20 @@ package pnj.project.topcalc;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.os.Handler;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.Toast;
 
 public class TCTermButton extends TCButton{
+	
+	String label;
+	String description;
+	String term;
+	Type type;
+	
 	
 	public TCTermButton(Context context) {
 	    super(context);
@@ -27,10 +38,6 @@ public class TCTermButton extends TCButton{
 		NON_MINUS_OPERATOR, DECIMAL, INTEGER, MINUS_OPERATOR, LEFT_BRACKET, RIGHT_BRACKET
 	}
 	
-	String label;
-	String description;
-	String term;
-	Type type;
 	
 	
 	
@@ -61,6 +68,42 @@ public class TCTermButton extends TCButton{
 			return true;
 		}
 		return true;
+	}
+	
+	Handler handel = new Handler();
+	
+	Runnable run = new Runnable() {
+
+	    @Override
+	    public void run() {
+	        Toast.makeText(getContext(), description, Toast.LENGTH_LONG).show();
+
+	    }
+	};
+	
+	@Override
+    public void setOnTouchListener(final OnTouchListener l) {
+	 super.setOnTouchListener(new OnTouchListener() {
+		 
+
+		@Override
+		public boolean onTouch(View arg0, MotionEvent arg1) {
+			switch (arg1.getAction()) {
+	         case MotionEvent.ACTION_DOWN:
+	             handel.postDelayed(run, 5000/* OR the amount of time you want */);
+	             break;
+
+	         default:
+	             handel.removeCallbacks(run);
+	             break;
+
+	         }
+	         return true;
+		}
+
+	    });
+	 
+	 
 	}
 
 	@Override
